bf4670026c473c8b4f90b7d494c8c6081ffed77d
Jacob Archambault <jacob.archambault@waystar.com>
2021-01-21 13:17:40 -0500

    make MyConsole class and its method non-static

--- src/com/jacobarchambault/randomnumber/MyConsole.java
+++ src/com/jacobarchambault/randomnumber/MyConsole.java
@@ -1,11 +1,17 @@
 package com.jacobarchambault.randomnumber;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class MyConsole {
-	static void writeToConsole(String filePath) throws IOException {
+	String filePath;
+	public MyConsole(String filePath) {
+		this.filePath = filePath;
+	}
+	void writeTo() throws IOException {
 		System.out.println(Files.readString(Paths.get(filePath)));
 	}
 
--- src/com/jacobarchambault/randomnumber/Program.java
+++ src/com/jacobarchambault/randomnumber/Program.java
@@ -24,7 +24,7 @@ public class Program {
 	private static void tryWriteToConsole(String filePath) {
 		// The StringBuffer class allows you to append to a string of characters
 		try {
-			MyConsole.writeToConsole(filePath);
+			new MyConsole(filePath).writeTo();
 		} catch (FileNotFoundException e) {
 			JOptionPane.showMessageDialog(null, "File Not Found. Check the name of the file.");
 		} catch (IOException ioe) {
