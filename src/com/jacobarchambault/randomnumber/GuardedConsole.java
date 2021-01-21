fc72bc1b34e9357bbdf426e4cae15053c4a4eac9
Jacob Archambault <jacob.archambault@waystar.com>
2021-01-21 14:31:03 -0500

    Remove fileNotFound catch from tryWriteToConsole

--- src/com/jacobarchambault/randomnumber/GuardedConsole.java
+++ src/com/jacobarchambault/randomnumber/GuardedConsole.java
@@ -1,9 +1,6 @@
 package com.jacobarchambault.randomnumber;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import javax.swing.JOptionPane;
 
 class GuardedConsole {
 	MyConsole console;
@@ -16,10 +13,8 @@ class GuardedConsole {
 		// The StringBuffer class allows you to append to a string of characters
 		try {
 			console.writeTo();
-		} catch (FileNotFoundException e) {
-			JOptionPane.showMessageDialog(null, "File Not Found. Check the name of the file.");
 		} catch (IOException ioe) {
-			System.out.println("Error reading the file" + ioe);
+			System.out.println("Error reading the medium" + ioe);
 		}
 	}
 
