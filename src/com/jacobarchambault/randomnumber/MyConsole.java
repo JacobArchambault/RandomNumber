cdfb24135f59e64b5a0d090d13226a01a4804903
Jacob Archambault <jacob.archambault@waystar.com>
2021-01-21 14:16:57 -0500

    Have MyConsole implement Writer interface

--- src/com/jacobarchambault/randomnumber/MyConsole.java
+++ src/com/jacobarchambault/randomnumber/MyConsole.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-class MyConsole {
+class MyConsole implements Writer {
 	String filePath;
 
 	MyConsole(String filePath) {
 		this.filePath = filePath;
 	}
 
-	void writeTo() throws IOException {
+	public void writeTo() throws IOException {
 		System.out.println(Files.readString(Paths.get(filePath)));
 	}
 
