0dc2bf8d224bb92e6222d0419940edd39e9c3335
Jacob Archambault <jacob.archambault@waystar.com>
2021-01-21 14:35:47 -0500

    Delete GuardedConsole

--- src/com/jacobarchambault/randomnumber/Program.java
+++ src/com/jacobarchambault/randomnumber/Program.java
@@ -13,7 +13,7 @@ public class Program {
 	public static void main(String[] args) throws IOException {
 		new GuardedMedium(new RandomNumbersFile(new FileWriter(new File("numbers.txt")),
 				new RandomNumberString(new StringBuilder(), new Random()))).writeTo();
-		new GuardedConsole(new MyConsole("numbers.txt")).tryWriteToConsole();
+		new GuardedMedium(new MyConsole("numbers.txt")).writeTo();
 	}
 
 }
