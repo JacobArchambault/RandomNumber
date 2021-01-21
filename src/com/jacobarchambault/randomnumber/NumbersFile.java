320d27da7860963d77e60d0995f8c761d9f9a24b
Jacob Archambault <jacob.archambault@waystar.com>
2021-01-21 11:29:50 -0500

    Extract randomNumberString method in NumbersFile.java

--- src/com/jacobarchambault/randomnumber/NumbersFile.java
+++ src/com/jacobarchambault/randomnumber/NumbersFile.java
@@ -8,19 +8,26 @@ import java.util.Random;
 public class NumbersFile {
 
 	FileWriter writer;
+
 	public NumbersFile(FileWriter writer) {
 		this.writer = writer;
 	}
+
 	void write100RandomNumbers() throws IOException {
+		String numberString = randomNumberString();
+		writer.write(numberString);
+		// always close the stream
+		writer.close();
+	}
+
+	private String randomNumberString() {
 		Random random = new Random();
 		StringBuilder stringBuilder = new StringBuilder();
 		// Generate 100 random numbers
 		for (int i = 0; i < 100; i++) {
 			stringBuilder.append(String.valueOf(random.nextInt()) + "\n");
 		}
-		writer.write(stringBuilder.toString());
-		// always close the stream
-		writer.close();
+		return stringBuilder.toString();
 	}
 
 }
