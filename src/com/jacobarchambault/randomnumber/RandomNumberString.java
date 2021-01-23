6a971938a0df7b426dd0c91bdf39a6f195857dde
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-23 15:59:36 -0500

    Add console message to randomNumberString's generate method

--- src/com/jacobarchambault/randomnumber/RandomNumberString.java
+++ src/com/jacobarchambault/randomnumber/RandomNumberString.java
@@ -13,7 +13,8 @@ public class RandomNumberString {
 
 	String generate() {
 		// Generate 100 random numbers
-		for (int i = 0; i < 100; i++) {
+		for (int i = 1; i <= 100; i++) {
+			System.out.println("Writing #" + i);
 			stringBuilder.append(String.valueOf(random.nextInt()) + "\n");
 		}
 		return stringBuilder.toString();
